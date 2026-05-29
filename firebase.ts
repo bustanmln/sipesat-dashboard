305a4d5e68e01195cb6a634a8abee277cfe666ed
Bustan <bustanmaulana50@gmail.com>
2026-05-29 22:24:05 +0700

    chore: configure Firebase SDK and Realtime Database in firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA41vwqEnPN8YeEhOZgsM69hUONQ7bxvWI",
+  authDomain: "sipesat-b51a5.firebaseapp.com",
+  databaseURL: "https://sipesat-b51a5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sipesat-b51a5",
+  storageBucket: "sipesat-b51a5.firebasestorage.app",
+  messagingSenderId: "321065737845",
+  appId: "1:321065737845:web:f98000ab5e70cd668da23c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and export
+export const db = getDatabase(app);
+
+export default app;
